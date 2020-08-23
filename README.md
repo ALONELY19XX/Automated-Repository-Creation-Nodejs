844992c4bb9c9b9de9a5f4b91e742093dada4e88
Valentin Wagner <valentin.wagner1998@gmail.com>
2020-08-23 21:28:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Automate GitHub Repo Creation with Nodejs
+## Installation
+1. Clone Repository
+2. cd /path/to/repo
+3. `npm install`
+4. create a [GitHub Acces Token](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token) (GHAT) and check the following scope(s):
+
+![GitHub Acces Token -  Needed scope(s)](./assets/scopes.png)
+
+5. Store your generated GHAT in a environment variable `GITHUB_PERSONAL_ACCESS_TOKEN` (use same name!). You can simply create an `.env` file inside the root of the clones repository and insert `GITHUB_PERSONAL_ACCESS_TOKEN=732462bdfshv8734fhj` (replace 73246bd... with your token)
+6. run `node github-create.js -n=REPO_NAME ...`
+
+## Usage
+
+![usage](./assets/usage.png)
+
+## Example
+
+`node github-create.js -n=REPO_NAME -p=true -d=XYZ -l=mit -i=true -g=nanoc`
---
+++ github-create.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const axios = require('axios')
+const { ArgumentParser } = require('argparse')
+require('dotenv').config()
+
+const parser = new ArgumentParser({
+    description: 'GitHub Repo Creation Automation'
+})
+parser.add_argument('-n', '--name', { help: 'Name of repository to be created'})
+parser.add_argument('-i', '--init', { help: 'Create initial commit with README.md (Default: false)'})
+parser.add_argument('-p', '--private', { help: 'Create private repository (Default: false)'})
+parser.add_argument('-d', '--description', { help: 'Description of repository'})
+parser.add_argument('-g', '--gitignore', { help: 'Name of gitignore template (Default: none)'})
+parser.add_argument('-l', '--license', { help: 'License to use for this repository (Default: none)'})
+const args = parser.parse_args()
+
+const GITHUB_API_REQUEST_URL = 'https://api.github.com/user/repos'
+
+axios.post(GITHUB_API_REQUEST_URL, {
+    name: args.name,
+    'auto_init': args.init,
+    'private': args.private,
+    'description': args.description,
+    'gitignore_template': args.gitignore,
+    'license_template': args.license
+}, {
+    headers: {
+        'Authorization': `token ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+    }
+})
+.then(response => {
+    if (!response.isAxiosError) {
+        console.log(`Successfully created repository "${args.name}".`)
+        console.log('REPO >>> ' + response.data.html_url)
+    }
+})
+.catch(error => {
+    console.log(`[!] ${error.response.data.message} Server responded with: ${error.response.status} ${error.response.statusText}. Make sure a repository with this name doesn\'t already exist.`)
+})
